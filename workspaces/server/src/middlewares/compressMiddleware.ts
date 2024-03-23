cac4900fc9b655b416ff05d370c774d8a507f837
H524 <watasekoni6@gmail.com>
2024-03-23 05:10:22 +0000

    [fix]Content-Lengthを取り除く処理を削除

--- workspaces/server/src/middlewares/compressMiddleware.ts
+++ workspaces/server/src/middlewares/compressMiddleware.ts
@@ -20,7 +20,6 @@ export const compressMiddleware = createMiddleware(async (c, next) => {
 
       c.res = new Response(c.res.body?.pipeThrough(transform), c.res);
 
-      c.res.headers.delete('Content-Length');
       c.res.headers.append('Cache-Control', 'no-transform');
       c.res.headers.set('X-Content-Encoding', 'zstd');
       break;
