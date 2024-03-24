136830553a465f2c6eb1726eb1003411b35c0aad
H524 <watasekoni6@gmail.com>
2024-03-24 00:03:34 +0000

    [update]不要なアイコンの取得を削除

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,14 +1,24 @@
-import * as Icons from '@mui/icons-material';
+// import * as Icons from '@mui/icons-material';
+import {ArrowBack, NavigateNext, Close, Favorite, FavoriteBorder, Search} from '@mui/icons-material';
 
 type Props = {
   color: string;
   height: number;
-  type: keyof typeof Icons;
+  type: String;
   width: number;
+};
+
+const IconType = {
+  ArrowBack: ArrowBack,
+  NavigateNext: NavigateNext,
+  Close: Close,
+  Favorite: Favorite,
+  FavoriteBorder: FavoriteBorder,
+  Search: Search
 };
 
 export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
   // eslint-disable-next-line
-  const Icon = Icons[type];
+  const Icon = IconType[type];
   return <Icon style={{ color, height, width }} />;
 };
